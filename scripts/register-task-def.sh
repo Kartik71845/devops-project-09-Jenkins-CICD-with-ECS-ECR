1eb0561255ed68e7f0877a85fa6729981abd6b18
Kartik71845 <kartikrajkumarb@gmail.com>
2025-12-27 17:37:22 +0530

    mofied

--- scripts/register-task-def.sh
+++ scripts/register-task-def.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 set -e
-echo "DEBUG:"       
-echo "CLUSTER_NAME=$1"
-echo "TASK_FAMILY=$2"
-echo "IMAGE_URI=$3"
-echo "AWS_REGION=$4"
 
-echo "Registering new task definition..."
+CLUSTER_NAME=$1
+TASK_FAMILY=$2
+IMAGE_URI=$3
+AWS_REGION=$4
+
+echo "DEBUG:"
+echo "CLUSTER_NAME=$CLUSTER_NAME"
+echo "TASK_FAMILY=$TASK_FAMILY"
+echo "IMAGE_URI=$IMAGE_URI"
+echo "AWS_REGION=$AWS_REGION"
+
+echo "Fetching existing task definition..."
 
 aws ecs describe-task-definition \
   --task-definition "$TASK_FAMILY" \
   --region "$AWS_REGION" \
   --query 'taskDefinition' > old-task-def.json
 
-jq '
+echo "Updating container image in task definition..."
+
+jq --arg IMAGE "$IMAGE_URI" '
   del(
     .taskDefinitionArn,
     .revision,
@@ -23,8 +31,10 @@ jq '
     .registeredAt,
     .registeredBy
   )
-  | .containerDefinitions[0].image = "'"$IMAGE_URI"'"
+  | .containerDefinitions |= map(.image = $IMAGE)
 ' old-task-def.json > new-task-def.json
+
+echo "Registering new task definition..."
 
 aws ecs register-task-definition \
   --cli-input-json file://new-task-def.json \
